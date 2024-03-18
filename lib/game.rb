12b3beb6cca4f87aa59488d1a938c43f434244e9
Jonnie Nguyen <jonnienguyen17@gmail.com>
2024-03-18 16:40:59 -0500

    Added deck class with all cards

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,21 @@
 class Game
-  def initialize
+end
 
+
+class Deck
+  # Using standard French 56-cards deck
+  attr_accessor :complete_deck
+  def initalize()
+    @complete_deck = []
   end
+  def createInitialDeck
+    suits = ["Spades", "Hearts", "Diamonds", "Club"]
+    cards = ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    @complete_deck = suits.product(cards)
+  end
+end
+
+
+class Card
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,3 +4,15 @@ require 'game'
 
 RSpec.describe Game do
 end
+
+
+RSpec.describe Deck do
+  let(:deckEX) {Deck.new()}
+
+  describe "#createDeck" do
+      it "Check for correct number of cards created." do
+        deckEX.createInitialDeck
+        expect(deckEX.complete_deck.length).to eq 56
+      end
+  end
+end
